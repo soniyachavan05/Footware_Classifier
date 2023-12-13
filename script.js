131f3b8991a3042725b4750003e162a98a58504e
Soniya Chavan <147008238+soniyachavan05@users.noreply.github.com>
2023-12-12 23:51:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ function predict() {
         const formData = new FormData();
         formData.append('file', fileInput.files[0]);
 
+        // Update the URL to match your Flask server's address
+        const url = 'http://127.0.0.1:5000/predict';
+
         $.ajax({
             type: 'POST',
-            url: 'predict',
+            url: url,
             data: formData,
             contentType: false,
             processData: false,
